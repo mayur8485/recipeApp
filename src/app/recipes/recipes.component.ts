94946ce6d7deacca237413cce5895b0c64d70a5f
Mayuresh <mayuresh.takawane@dista.ai>
2024-05-28 22:12:58 +0530

    fetch recipes on onInit.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
-import { RecipeService } from './recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-recipes',
@@ -11,9 +10,10 @@ export class RecipesComponent implements OnInit {
   // selectedRecipe:Recipe
 
 
-  constructor(private recipeService:RecipeService) { }
+  constructor(private dataStoreService:DataStorageService) { }
 
   ngOnInit(): void {
+    this.dataStoreService.fetchRecipes().subscribe();
     // this.recipeService.recipeSelected.subscribe((recipe:Recipe)=>{
     //   this.selectedRecipe = recipe;
     // })
